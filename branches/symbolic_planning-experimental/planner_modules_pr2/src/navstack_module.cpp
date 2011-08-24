52a60986868ec9b0ca962722058a1181cea07769
c.dornhege@googlemail.com <c.dornhege@googlemail.com@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2011-08-24 14:06:08 +0000

    add pr2 navstack planner module

---
+++ branches/symbolic_planning-experimental/planner_modules_pr2/src/navstack_module.cpp
@@ -0,0 +1,194 @@
+#include "navstack_module.h"
+#include <ros/ros.h>
+#include <nav_msgs/GetPlan.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <map>
+using std::map;
+#include <utility>
+using std::pair; using std::make_pair;
+#include <boost/foreach.hpp>
+#define forEach BOOST_FOREACH
+#include <sys/times.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+
+/**
+ * Dummy module implementation for ROS navigation domain.
+ *
+ * Directly queries the move_base_node/make_plan service for each
+ * cost request by the planner.
+ */
+
+static const bool s_Debug = false;
+
+static ros::NodeHandle* s_NodeHandle = NULL;
+static ros::ServiceClient s_GetPlan;
+
+/// Plan requests are issued using this frame - so the poses from the planner are given in this frame (e.g. map)
+static std::string s_WorldFrame;
+
+static double s_GoalTolerance = 0.5;
+
+// Using a cache of queried path costs to prevent calling the path planning service multiple times
+// Better: Can we assume symmetric path costs?
+static map< pair<string,string>, double> s_PathCostCache;
+
+void navstack_init(int argc, char** argv)
+{
+   ROS_ASSERT(argc == 2);
+  
+   ros::NodeHandle nhPriv("~");
+   std::string tfPrefix = tf::getPrefixParam(nhPriv);
+   s_WorldFrame = tf::resolve(tfPrefix, argv[1]);
+   ROS_INFO("World frame is: %s", s_WorldFrame.c_str());
+
+   // init service query
+   s_NodeHandle = new ros::NodeHandle();
+
+   nhPriv.param("goal_tolerance", s_GoalTolerance, s_GoalTolerance);
+   ROS_INFO("Goal Tolerance is: %f.", s_GoalTolerance);
+
+   while(!ros::service::waitForService("move_base_node/make_plan", ros::Duration(3.0))) {
+      ROS_ERROR("Service move_base_node/make_plan not available - waiting.");
+   }
+
+   s_GetPlan = s_NodeHandle->serviceClient<nav_msgs::GetPlan>("move_base_node/make_plan", true);
+   if(!s_GetPlan) {
+      ROS_FATAL("Could not initialize get plan service from move_base_node/make_plan (client name: %s)", s_GetPlan.getService().c_str());
+   }
+
+   ROS_INFO("Initialized Navstack Module.\n");
+}
+
+double pathCost(const ParameterList & parameterList, 
+      predicateCallbackType predicateCallback, numericalFluentCallbackType numericalFluentCallback,
+      int relaxed, plannerContextPtr context, plannerContextCompareType contextComp, bool & tookContext)
+{
+   if(s_Debug) {        // prevent spamming ROS_DEBUG calls unless we really want debug
+      // debugging raw planner calls
+      static unsigned int calls = 0;
+      calls ++;
+      if(calls % 10000 == 0) {
+         ROS_DEBUG("Got %d module calls.\n", calls);
+      }
+   }
+
+   // first lookup in the cache if we answered the query already
+   map<pair<string,string>, double>::iterator it = s_PathCostCache.find(make_pair(parameterList[0].value, parameterList[1].value));
+   if(it != s_PathCostCache.end()) {
+      return it->second;
+   }
+   
+   // get robot and target location from planner interface
+   ROS_ASSERT(parameterList.size() == 2);
+
+   ParameterList startParams;
+   startParams.push_back(parameterList[0]);
+   ParameterList goalParams;
+   goalParams.push_back(parameterList[1]);
+   NumericalFluentList nfRequest;
+   nfRequest.reserve(14);
+   nfRequest.push_back(NumericalFluent("x", startParams));
+   nfRequest.push_back(NumericalFluent("y", startParams));
+   nfRequest.push_back(NumericalFluent("z", startParams));
+   nfRequest.push_back(NumericalFluent("qx", startParams));
+   nfRequest.push_back(NumericalFluent("qy", startParams));
+   nfRequest.push_back(NumericalFluent("qz", startParams));
+   nfRequest.push_back(NumericalFluent("qw", startParams));
+   nfRequest.push_back(NumericalFluent("x", goalParams));
+   nfRequest.push_back(NumericalFluent("y", goalParams));
+   nfRequest.push_back(NumericalFluent("z", goalParams));
+   nfRequest.push_back(NumericalFluent("qx", goalParams));
+   nfRequest.push_back(NumericalFluent("qy", goalParams));
+   nfRequest.push_back(NumericalFluent("qz", goalParams));
+   nfRequest.push_back(NumericalFluent("qw", goalParams));
+
+   NumericalFluentList* nfRequestP = &nfRequest;
+   if(!numericalFluentCallback(nfRequestP)) {
+      ROS_ERROR("numericalFluentCallback failed.");
+      return INFINITE_COST;
+   }
+
+   // create the path planning query for service
+   nav_msgs::GetPlan srv;
+   srv.request.start.header.frame_id = s_WorldFrame;
+   srv.request.goal.header.frame_id = s_WorldFrame;
+   srv.request.start.pose.position.x = nfRequest[0].value;
+   srv.request.start.pose.position.y = nfRequest[1].value;
+   srv.request.start.pose.position.z = nfRequest[2].value;
+   srv.request.start.pose.orientation.x = nfRequest[3].value;
+   srv.request.start.pose.orientation.y = nfRequest[4].value;
+   srv.request.start.pose.orientation.z = nfRequest[5].value;
+   srv.request.start.pose.orientation.w = nfRequest[6].value;
+   srv.request.goal.pose.position.x = nfRequest[7].value;
+   srv.request.goal.pose.position.y = nfRequest[8].value;
+   srv.request.goal.pose.position.z = nfRequest[9].value;
+   srv.request.goal.pose.orientation.x = nfRequest[10].value;
+   srv.request.goal.pose.orientation.y = nfRequest[11].value;
+   srv.request.goal.pose.orientation.z = nfRequest[12].value;
+   srv.request.goal.pose.orientation.w = nfRequest[13].value;
+   srv.request.tolerance = s_GoalTolerance;
+   
+   double cost = INFINITE_COST;
+
+   if(!s_GetPlan) {
+      ROS_ERROR("Persisten service connection to %s failed.", s_GetPlan.getService().c_str());
+      // FIXME reconnect - this shouldn't happen.
+      return INFINITE_COST;
+   }
+
+   // statistics about using the ros path planner service
+   static double plannerCalls = 0;
+   static ros::Duration totalCallsTime = ros::Duration(0.0);
+   plannerCalls += 1.0;
+
+   ros::Time callStartTime = ros::Time::now();
+   // This construct is here, because when the robot is moving move_base will not produce other paths
+   // we retry for a certain amount of time to not fail directly.
+   // FIXME: Cleanup the goto code
+retryGetPlan:
+   static unsigned int failCounter = 0;
+
+   // perform the actual path planner call
+   if(s_GetPlan.call(srv)) {
+      failCounter = 0;
+
+      if(s_Debug) {
+         ros::Time callEndTime = ros::Time::now();
+         ros::Duration dt = callEndTime - callStartTime;
+         totalCallsTime += dt;
+         ROS_DEBUG("ServiceCall took: %f, avg: %f (num %f).", dt.toSec(), totalCallsTime.toSec()/plannerCalls, plannerCalls);
+      }
+
+      if(!srv.response.plan.poses.empty()) {
+         // get plan cost
+         double pathLength = 0;
+         geometry_msgs::PoseStamped lastPose = srv.response.plan.poses[0];
+         forEach(const geometry_msgs::PoseStamped & p, srv.response.plan.poses) {
+            double d = hypot(lastPose.pose.position.x - p.pose.position.x, 
+                  lastPose.pose.position.y - p.pose.position.y);
+            pathLength += d;
+            lastPose = p;
+         }
+         cost = pathLength;
+      }
+
+      ROS_INFO("Got plan from service.");
+      // also empty plan = OK or fail or none?
+   } else {
+      ROS_ERROR("Failed to call service %s - is the robot moving?", s_GetPlan.getService().c_str());
+      failCounter++;
+      if(failCounter < 30) {
+         usleep(1000*1000);
+         goto retryGetPlan;
+      }
+      // FIXME: what if target is unreachable, do we get false or an empty plan? i.e. is this an error
+      return INFINITE_COST;
+   }
+
+   // return pathcost and cache
+   s_PathCostCache[ make_pair(parameterList[0].value, parameterList[1].value) ] = cost;
+
+   return cost;
+}
+
