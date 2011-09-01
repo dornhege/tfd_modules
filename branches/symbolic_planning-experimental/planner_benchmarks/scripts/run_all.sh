6e977cb060619f74300c8c244c4fb2755e24906f
c.dornhege@googlemail.com <c.dornhege@googlemail.com@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2011-09-01 13:55:28 +0000

    add eval script for unit tests

--- branches/symbolic_planning-experimental/planner_benchmarks/scripts/run_all.sh
+++ branches/symbolic_planning-experimental/planner_benchmarks/scripts/run_all.sh
@@ -6,7 +6,7 @@ TODAY=$(date +%Y-%m-%d)
 echo REVISION: $LATEST_REVISION TODAY: $TODAY
 echo Warning: Make sure that all directories are up2date with this revision and there are no outgoings
 
-CONFIGS="tfd_modules_params_no_anytime.yaml tfd_modules_params_timeout_10_30.yaml"
+CONFIGS="tfd_modules_params_no_anytime_60s.yaml tfd_modules_params_timeout_10_30.yaml"
 PROBLEMS="problem_sets/crewplanning.dat problem_sets/elevators-strips.dat problem_sets/openstacks-adl.dat problem_sets/pegsol-strips.dat problem_sets/transport.dat"
 
 CONFIG_DIR=$(rospack find tfd_modules)/config/
