95b581cd628af44743c56c1be49c1677ad311a27
c.dornhege@googlemail.com <c.dornhege@googlemail.com@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2011-09-02 14:43:44 +0000

    TFD: eval script compares to ref data

--- branches/symbolic_planning-experimental/planner_benchmarks/scripts/eval.py
+++ branches/symbolic_planning-experimental/planner_benchmarks/scripts/eval.py
@@ -11,6 +11,8 @@ import shutil
 import collections
 import datetime
 
+import data_tools
+
 class Problem(object):
     """ An evaluated problem with a plan and a times debug file. """
     def __init__(self, path, name):
@@ -43,46 +45,29 @@ class Problem(object):
                 yield (float)(entries[0])
                 yield (float)(entries[1])
 
-def make_dir_list(path):
-    """ Create list from path where each directory is one list entry """
-    front,back = os.path.split(path)
-    if not back and not front:
-        return []
-    if not back:
-        return make_dir_list(front)
-    if not front:
-        return [ back ]
-    frontL = make_dir_list(front)
-    backL = make_dir_list(back)
-    if not frontL:
-        return backL
-    if not backL:
-        return frontL
-    theList = []
-    theList.extend(frontL)
-    theList.extend(backL)
-    return theList
-
 def evalDir(path, files):
+    """ Evaluate the directory in path that contains a number of
+        plan...pddl.best files.
+        Returns a dict mapping from path to a list
+        of Problems """
     # The expected format for problem named X with problem file X.pddl is:
     # plan.X.pddl.best and times.X.pddl
     plannames = []
     for f in files:
         if f.endswith(".pddl.best"):
+            # plan.p03.pddl.best -> p03
             assert(f.startswith("plan."))
             assert(len(f) > len("plan.") + len(".pddl.best"))
-            name = f[len("plan."): - len(".pddl.best")]
+            name = f[len("plan.") : - len(".pddl.best")]
             plannames.append(name)
     plannames.sort(key=str.lower)
     # valid names are those, where a times.X.pddl file exists
     validnames = [name for name in plannames if os.path.exists(os.path.join(path, "times." + name + ".pddl"))]
-    problems = []
-    for name in validnames:
-        problems.append(Problem(path, name))
-    
+    problems = [Problem(path, name) for name in validnames]
+ 
     # create return dictionary from path A/B/C/files -> {A: {B: {C: problems} } }
     theDict = {}
-    dirList = make_dir_list(path)
+    dirList = data_tools.make_dir_list(path)
 
     # seed dict with lowest level containing the problems
     assert len(dirList) > 0
@@ -98,63 +83,27 @@ def evalDir(path, files):
 
     return theDict
 
-def quacks_like_dict(object):
-    """Check if object is dict-like"""
-    return isinstance(object, collections.Mapping)
-    
-# From: http://appdelegateinc.com/blog/2011/01/12/merge-deeply-nested-dicts-in-python/
-def merge(a, b):
-    """Merge two deep dicts non-destructively
-    
-    Uses a stack to avoid maximum recursion depth exceptions
-    
-    >>> a = {'a': 1, 'b': {1: 1, 2: 2}, 'd': 6}
-    >>> b = {'c': 3, 'b': {2: 7}, 'd': {'z': [1, 2, 3]}}
-    >>> c = merge(a, b)
-    >>> from pprint import pprint; pprint(c)
-    {'a': 1, 'b': {1: 1, 2: 7}, 'c': 3, 'd': {'z': [1, 2, 3]}}
-    """
-    assert quacks_like_dict(a), quacks_like_dict(b)
-    dst = a.copy()
-    
-    stack = [(dst, b)]
-    while stack:
-        current_dst, current_src = stack.pop()
-        for key in current_src:
-            if key not in current_dst:
-                current_dst[key] = current_src[key]
-            else:
-                if quacks_like_dict(current_src[key]) and quacks_like_dict(current_dst[key]) :
-                    stack.append((current_dst[key], current_src[key]))
-                else:
-                    current_dst[key] = current_src[key]
-    return dst
-
-def parseResults(results_dir):
+def parseResults(eval_dir):
     """ Parse dir and all subdirectories to create results. 
         Returns a dictionary of the directory structure relative
-        to results_dir with the evaluated problems """
+        to eval_dir with the evaluated problems """
     # List directories, a problem/domain directory contains one or more files with "pddl"
 
     allResults = {}
-    for root, dirs, files in os.walk(results_dir):
-        #print "Root: ", root
-        #print "Dirs: ", dirs
-        #print "Files: ", files
-        #print ""
+    for root, dirs, files in os.walk(eval_dir):
         # If files contains a file with "pddl" in it, it is a result dir
         pddls = [f for f in files if f.lower().find("pddl") >= 0]
         if pddls:
             dirEval = evalDir(root, files)
-            #print "DIR EVAL for ", root, " : ", dirEval
-            allResults = merge(allResults, dirEval)
-            #print "ALL RESULTS: ", allResults
+            allResults = data_tools.merge(allResults, dirEval)
     return allResults
 
 def writeEvalData(evaldict, path):
+    """ write eval.dat with evaluation for all problems
+        in each domain directory """
     for key, val in evaldict.iteritems():
         localpath = os.path.join(path, key)
-        if quacks_like_dict(val):   # recurse
+        if data_tools.behaves_like_dict(val):   # recurse
             writeEvalData(val, localpath)
         else:   # list of problems
             f = file(os.path.join(localpath, "eval.dat"), "w")
@@ -163,46 +112,80 @@ def writeEvalData(evaldict, path):
             f.close()
 
 def buildNameIdentifierDict(evaldict, name_entries_dict, curIdent):
+    """ From a dictionary like {K: {A: {X:1 Y:2}, B: {X:1 Z:3}}}
+        build a dictionary: {X: {"K/A":1 "K/B": 1} Y: {"K/A": 2} Z: {"K/B" : 3}} """
     for key, val in evaldict.iteritems():
-        if quacks_like_dict(val):   # recurse
+        if data_tools.behaves_like_dict(val):   # recurse
             buildNameIdentifierDict(val, name_entries_dict, curIdent + "/" + key)
         else:           # found final dict that contains name -> problems
-            if not key in name_entries_dict:
+            if not key in name_entries_dict:    # init
                 name_entries_dict[key] = {}
             name_entries_dict[key][curIdent] = val
 
-def writeTex(evaldict, filename):
-    """ Write latex table for this dict.
+def writeTex(evaldict, filename, refDict):
+    """ Write latex file for this dict.
         For dict: {X: {A: problems, B: problems}, Y: {A: problems, C: problems}}
         the output should be a table for A, B, C, where A has cols X/Y, B col X, C col Y
         """
     # First build the name -> {identifier -> entries} dict.
     nameEntriesDict = {}
     buildNameIdentifierDict(evaldict, nameEntriesDict, "")
-    #print nameEntriesDict
+
+    refEntriesDict = None
+    if refDict:
+        refEntriesDict = {}
+        buildNameIdentifierDict(refDict, refEntriesDict, "")
 
     f = open(filename, "w")
     print >> f, '\\documentclass{article}'
     print >> f, "\\usepackage{caption}"
     print >> f, '\\begin{document}'
 
-    writeTexTable(nameEntriesDict, f, "makespan", "<")
-    writeTexTable(nameEntriesDict, f, "runtime", "<")
+    if not refEntriesDict:      # runtime doesnt make sense for ref data unless we have some info
+        writeTexTable(nameEntriesDict, f, "makespan", "<", refEntriesDict)
+        writeTexTable(nameEntriesDict, f, "runtime", "<", refEntriesDict)
+    else:
+        writeTexTable(nameEntriesDict, f, "makespan", ">", refEntriesDict)
 
     print >> f, '\\end{document}'
     f.flush()
     f.close()
 
-def writeTexTable(nameEntriesDict, f, target, better):
-    for key, val in nameEntriesDict.iteritems():
-        print "Domain: ", key
+def writeTexTable(nameEntriesDict, f, target, better, refEntriesDict):
+    """ Write latex table for this dict.
+        Creates a table that has one row per problem.
+        There is one column per Setting/Version.
+        Target gives the target property of a problem to write in a column.
+        If comparing the targets with better and item is equal to
+        the best, the entry is marked bold.
+        """
+    for domain, val in nameEntriesDict.iteritems():    # domain -> {Run -> problems}
+        refVals = None
+        if refEntriesDict:
+            try:
+                refVals = refEntriesDict[domain]
+                # don't really care about the ident for ref data, so just go down
+                # in dict until we have the problem list
+                while data_tools.behaves_like_dict(refVals):
+                    assert len(refVals) == 1        # there should only be one ref data per domain
+                    for rkey, rval in refVals.iteritems():
+                        refVals = rval
+                        break
+                print "REF vals for domain", domain, "is", refVals
+            except:
+                print "WARNING: No reference data for domain", domain, "- skipping domain!"
+                continue
         print >> f, '\\begin{table}'
         print >> f, '  \\centering'
         print >> f, '  \\begin{tabular}{|l|c|c|}'
         print >> f, '  \\hline'
+        
+        # Runs contains:  Unique number, Descriptor Ident, Problem list
+        runs = [ (num, ident, probs) for num, (ident,probs) in enumerate(val.iteritems()) ]
+
         # Write table header
         print >> f, '  Problem & ',
-        for num, (ident, probs) in enumerate(val.iteritems()):  # is the num ordering somewhat guaranteed?
+        for num, ident, probs in runs:
             print >> f, num,
             if num < len(val) - 1:
                 print >> f, "&",
@@ -210,47 +193,81 @@ def writeTexTable(nameEntriesDict, f, target, better):
                 print >> f, ""
         print >> f, "\\\\"
         print >> f, '  \\hline'
-        problems = set()
+        
         # First collect all problems (some might not have entries)
-        for num, (ident, probs) in enumerate(val.iteritems()):  # is the num ordering somewhat guaranteed?
-            print "NUM", num
-            print "RunIdnet: ", ident
-            print "Problems: ", probs
+        problems = set()
+        for num, ident, probs in runs:
             for i in probs:
                 problems.add(repr(i))
-        print "PROB SET ", problems
         probList = list(problems)
         probList.sort(key=str.lower)
-        # write table lines
+
+        if refVals:
+            probWithRefList = []
+            for i in probList:
+                refProbs = [p for p in refVals if repr(p) == i]
+                if not refProbs:
+                    print "No Ref data for domain", domain, " problem:", repr(i), "- skipping"
+                    continue
+                assert len(refProbs) == 1
+                probWithRefList.append(i)
+            probList = probWithRefList
+
+        # Now for each problem, write a table line
         for i in probList:
-            print "Problem: ", i
             print >> f, i, "    &",
             best = None
-            # best
-            for num, (ident, probs) in enumerate(val.iteritems()):
+            
+            targetStr = "myProb[0]." + target
+            refProbStr = "refProb[0]." + target
+            # find best entry in line
+            for num, ident, probs in runs:
                 myProb = [j for j in probs if repr(j) == i]
-                if myProb:
-                    assert len(myProb) == 1
-                    if not best:
-                        best = eval("myProb[0]." + target)
-                    else:
-                        if eval("myProb[0]." + target + " " + better + " best"):
-                            best = eval("myProb[0]." + target)
-
-            for num, (ident, probs) in enumerate(val.iteritems()):
-                print "NUM", num
-                print "RunIdnet: ", ident
-                print "Problems: ", probs
-                myProb = [j for j in probs if repr(j) == i]
-                if myProb:
-                    assert len(myProb) == 1
-                    if best and best == eval("myProb[0]." + target):
-                        print >> f, "\\textbf{",
-                    print >> f, eval("myProb[0]." + target),
-                    if best and best == eval("myProb[0]." + target):
-                        print >> f, "}",
+                if refVals:
+                    refProb = [j for j in refVals if repr(j) == i]
+                    assert refProb
+                    if myProb:
+                        assert len(myProb) == 1
+                        if not best:
+                            best = eval(refProbStr + " / " + targetStr)
+                        else:
+                            if eval(refProbStr + " / " + targetStr + " " + better + " best"):
+                                best = eval(refProbStr + " / " + targetStr)
                 else:
-                    print >> f, "- ",
+                    if myProb:
+                        assert len(myProb) == 1
+                        if not best:
+                            best = eval(targetStr)
+                        else:
+                            if eval(targetStr + " " + better + " best"):
+                                best = eval(targetStr)
+
+            # write actual entry for each ident
+            for num, ident, probs in runs:
+                myProb = [j for j in probs if repr(j) == i]
+                if refVals:
+                    refProb = [j for j in refVals if repr(j) == i]
+                    assert refProb
+                    if myProb:
+                        assert len(myProb) == 1
+                        if best and best == eval(refProbStr + " / " + targetStr):
+                            print >> f, "\\textbf{",
+                        print >> f, "%.2f" % eval(refProbStr + " / " + targetStr),
+                        if best and best == eval(refProbStr + " / " + targetStr):
+                            print >> f, "}",
+                    else:
+                        print >> f, " - ",
+                else:
+                    if myProb:
+                        assert len(myProb) == 1
+                        if best and best == eval(targetStr):
+                            print >> f, "\\textbf{",
+                        print >> f, "%.2f" % eval(targetStr),
+                        if best and best == eval(targetStr):
+                            print >> f, "}",
+                    else:                       # no entry for this run version -> "-"
+                        print >> f, " - ",
+
                 if num < len(val) - 1:
                     print >> f, "&",
                 else:
@@ -259,9 +276,10 @@ def writeTexTable(nameEntriesDict, f, target, better):
 
         print >> f, '  \\hline'
         print >> f, '  \\end{tabular}'
-        print >> f, '  \\caption{\\textbf{', target, '} Domain: \\textbf{', key, '}',
+        print >> f, '  \\caption{\\textbf{', target, '} Domain: \\textbf{', domain, '}',
         print >> f, "\\\\"
-        for num, (ident, probs) in enumerate(val.iteritems()):
+        # write descriptor in caption matching run# to a description
+        for num, ident, probs in runs:
             print >> f, str(num) + ": ", ident.replace("_", "\\_") + ",",
             print >> f, "\\\\"
         print >> f, ' }'
@@ -271,18 +289,27 @@ def writeTexTable(nameEntriesDict, f, target, better):
 
 def main():
     parser = OptionParser("usage: %prog PROBLEMS")
-    parser.add_option("-r", "--results-dir", dest="results_dir", type="string", action="store")
+    parser.add_option("-e", "--eval-dir", dest="eval_dir", type="string", action="store")
+    parser.add_option("-r", "--ref-data-dir", dest="ref_data_dir", type="string", action="store")
     opts, args = parser.parse_args()
-    print "Results dir: %s" % opts.results_dir
+    print "Eval results dir: %s" % opts.eval_dir
+    print "Ref data dir: %s" % opts.ref_data_dir
 
-    evalDict = parseResults(opts.results_dir)
-    print "FINAL EVAL DICT: ", evalDict
+    evalDict = parseResults(opts.eval_dir)
+    print "FINAL EVAL DICT: ", evalDict[opts.eval_dir]
+
+    ref_data = None
+    if opts.ref_data_dir:
+        ref_data = parseResults(opts.ref_data_dir)
+        print "REF DATA DICT: ", ref_data[opts.ref_data_dir]
+
+    # TODO then create compare dict and start that mode (cleanup structures before)
 
     # write eval data
     writeEvalData(evalDict, ".")
 
     # create latex tables, all grouped by domain-name divided by settings/algos
-    writeTex(evalDict, "output.tex")
+    writeTex(evalDict, "output.tex", ref_data)
 
 if __name__ == "__main__":
     main()
