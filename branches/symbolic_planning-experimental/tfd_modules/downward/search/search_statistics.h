7d6fec498bbfa9e489f9d16fc2b299076e2d0c42
c.dornhege@googlemail.com <c.dornhege@googlemail.com@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2011-10-10 17:05:50 +0000

    TFD: moved statistics code into own class

--- branches/symbolic_planning-experimental/tfd_modules/downward/search/search_statistics.h
+++ branches/symbolic_planning-experimental/tfd_modules/downward/search/search_statistics.h
@@ -8,8 +8,6 @@ class SearchStatistics
         ~SearchStatistics();
 
         int generated_states;
-        int statePutInOpenList;
-        int statesOmmitedFromOpenList;
         int childsWithSameG;
         int childsWithDifferentG;
         int childsWithSameF;
@@ -19,6 +17,19 @@ class SearchStatistics
         int parentsWithAtMostOneChild;
         int parentsWithAtMostOneChildWithSameG;
 
+        /// Count statistics for one generated child
+        void countChild(double parentG, double childG, double parentF, double childF);
+
+        /// This should be called after the childs of one node have been expanded and counted to compute extra stats.
+        void finishExpansion();
+    
+        /// Output statistics
+        void dump(unsigned int closedListSize) const;
+
+    private:
+        // internal counters per expansion
+        int numberOfChildren;
+        int numberOfChildrenWithSameG;
 };
 
 #endif
