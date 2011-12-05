0653f7fe3654ebcc1a894a8fad461b4ccd7ccb3d
c.dornhege@googlemail.com <c.dornhege@googlemail.com@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2011-12-05 19:24:15 +0000

    add actions for tidyup_grasp.

---
+++ branches/symbolic_planning-experimental/tidyup_grasp_actions/src/actionExecutorRequestGraspableObjects.cpp
@@ -0,0 +1,51 @@
+#include "tidyup_grasp_actions/actionExecutorRequestGraspableObjects.h"
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_DECLARE_CLASS(tidyup_grasp_actions, action_executor_request_graspable_objects,
+        tidyup_grasp_actions::ActionExecutorRequestGraspableObjects,
+        continual_planning_executive::ActionExecutorInterface)
+
+namespace tidyup_grasp_actions
+{
+
+    bool ActionExecutorRequestGraspableObjects::fillGoal(tidyup_msgs::RequestGraspableObjects::Request & goal,
+            const DurativeAction & a, const SymbolicState & current)
+    {
+        goal.onlyReachable = true;
+        return true;
+    }
+
+    void ActionExecutorRequestGraspableObjects::updateState(bool success,
+            tidyup_msgs::RequestGraspableObjects::Response & response,
+            const DurativeAction & a, SymbolicState & current)
+    {
+        if(!success)
+            return;
+
+        ROS_ASSERT(a.parameters.size() == 1);
+        std::string location = a.parameters.at(0);
+
+        for(std::vector<tidyup_msgs::GraspableObject>::iterator it = response.objects.begin();
+                it != response.objects.end(); it++) {
+            tidyup_msgs::GraspableObject & object = *it;
+
+            current.addObject(object.name, "movable_object");
+            current.addObject(object.pose.header.frame_id, "frameid");
+            current.setBooleanPredicate("graspable-from", object.name + " " + location, true);
+            current.setNumericalFluent("x", object.name, object.pose.pose.position.x);
+            current.setNumericalFluent("y", object.name, object.pose.pose.position.y);
+            current.setNumericalFluent("z", object.name, object.pose.pose.position.z);
+            current.setNumericalFluent("qx", object.name, object.pose.pose.orientation.x);
+            current.setNumericalFluent("qy", object.name, object.pose.pose.orientation.y);
+            current.setNumericalFluent("qz", object.name, object.pose.pose.orientation.z);
+            current.setNumericalFluent("qw", object.name, object.pose.pose.orientation.w);
+            current.setNumericalFluent("timestamp", object.name, object.pose.header.stamp.toSec());
+            current.setObjectFluent("frame-id", object.name, object.pose.header.frame_id);
+            ROS_ASSERT(object.reachable);
+        }
+
+        current.setBooleanPredicate("detected-objects", location, true);
+    }
+
+};
+
---
+++ branches/symbolic_planning-experimental/tidyup_grasp_actions/src/goalCreatorGraspObject.cpp
@@ -0,0 +1,62 @@
+#include "tidyup_grasp_actions/goalCreatorGraspObject.h"
+#include "hardcoded_facts/geometryPoses.h"
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
+
+PLUGINLIB_DECLARE_CLASS(tidyup_grasp_actions, goal_creator_grasp_object,
+        tidyup_grasp_actions::GoalCreatorGraspObject, continual_planning_executive::GoalCreator)
+
+namespace tidyup_grasp_actions 
+{
+
+    GoalCreatorGraspObject::GoalCreatorGraspObject()
+    {
+    }
+
+    GoalCreatorGraspObject::~GoalCreatorGraspObject()
+    {
+    }
+
+    bool GoalCreatorGraspObject::fillStateAndGoal(SymbolicState & currentState, SymbolicState & goal)
+    {
+        ros::NodeHandle nhPriv("~");
+
+        // load goal locations
+        std::string goalLocationsFile;
+        if(!nhPriv.getParam("goal_locations", goalLocationsFile)) {
+            ROS_ERROR("Could not get ~goal_locations parameter.");
+            return false;
+        }
+
+        // at the targets to state and goal
+        GeometryPoses goalLocations;
+        if(!goalLocations.load(goalLocationsFile)) {
+            ROS_ERROR("Could not load goal locations from \"%s\".", goalLocationsFile.c_str());
+            return false;
+        }
+        // create the actual states
+        const std::map<std::string, geometry_msgs::Pose> & goalPoses = goalLocations.getPoses();
+        forEach(const GeometryPoses::NamedPose & np, goalPoses) {
+            currentState.addObject(np.first, "grasp_location");
+            goal.addObject(np.first, "grasp_location");
+
+            currentState.setNumericalFluent("x", np.first, np.second.position.x);
+            currentState.setNumericalFluent("y", np.first, np.second.position.y);
+            currentState.setNumericalFluent("z", np.first, np.second.position.z);
+            currentState.setNumericalFluent("qx", np.first, np.second.orientation.x);
+            currentState.setNumericalFluent("qy", np.first, np.second.orientation.y);
+            currentState.setNumericalFluent("qz", np.first, np.second.orientation.z);
+            currentState.setNumericalFluent("qw", np.first, np.second.orientation.w);
+            // make them clean
+            goal.setBooleanPredicate("clean", np.first, true);
+        }
+
+        // a bit hacky: init currentState here
+        currentState.addObject("right_arm", "arm");
+        currentState.setBooleanPredicate("handFree", "right_arm", true);
+
+        return true;
+    }
+
+};
+
