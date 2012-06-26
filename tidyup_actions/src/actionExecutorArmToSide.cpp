6fcef96f7798338244009f8576862e7a4e7b3bbc
Christian Dornhege <dornhege@users.noreply.github.com>
2012-06-26 15:47:47 +0000

    generalized actionExecutorArmToSide and moved it to tidyup_actions

--- tidyup_actions/src/actionExecutorArmToSide.cpp
+++ tidyup_actions/src/actionExecutorArmToSide.cpp
@@ -1,12 +1,28 @@
-#include "tidyup_place_actions/actionExecutorArmToSide.h"
+#include "tidyup_actions/actionExecutorArmToSide.h"
 #include <pluginlib/class_list_macros.h>
 
-PLUGINLIB_DECLARE_CLASS(tidyup_place_actions, action_executor_arm_to_side,
-        tidyup_place_actions::ActionExecutorArmToSide,
+PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_arm_to_side,
+        tidyup_actions::ActionExecutorArmToSide,
         continual_planning_executive::ActionExecutorInterface)
 
-namespace tidyup_place_actions
+namespace tidyup_actions
 {
+
+    void ActionExecutorArmToSide::initialize(const std::deque<std::string> & arguments)
+    {
+        ActionExecutorActionlib<tidyup_msgs::ArmToSideAction, tidyup_msgs::ArmToSideGoal,
+            tidyup_msgs::ArmToSideResult>::initialize(arguments);
+
+        _armStatePredicateName = "arm-state";
+        _armAtSideConstantName = "arm_at_side";
+
+        if(arguments.size() >= 3) {     // 3rd param: arm-state predicate name
+            _armStatePredicateName = arguments[2];
+        }
+        if(arguments.size() >= 4) {     // 4th param: arm_at_side constant name
+            _armAtSideConstantName = arguments[3];
+        }
+    }
 
     bool ActionExecutorArmToSide::fillGoal(tidyup_msgs::ArmToSideGoal & goal,
                         const DurativeAction & a, const SymbolicState & current)
@@ -32,7 +48,7 @@ namespace tidyup_place_actions
             ROS_INFO("Arm Side Position succeeded.");
             ROS_ASSERT(a.parameters.size() == 1);
             string arm = a.parameters[0];
-            current.setObjectFluent("arm-position", arm, "arm_at_side");
+            current.setObjectFluent(_armStatePredicateName, arm, _armAtSideConstantName);
         }
     }
 
