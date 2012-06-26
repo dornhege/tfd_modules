71d09e2358812cf07cd994ebec4f71885636f5ae
Andreas Hertle <ahertle@users.noreply.github.com>
2012-06-26 16:52:55 +0000

    tidyup_action: updated action executors according to tidyup-putdown/domain-modules-doors.pddl

--- tidyup_actions/src/actionExecutorDriveThroughDoor.cpp
+++ tidyup_actions/src/actionExecutorDriveThroughDoor.cpp
@@ -1,27 +1,76 @@
-#include "tidyup_actions/actionExecutorMoveBase.h"
+#include "tidyup_actions/actionExecutorDriveThroughDoor.h"
 #include <pluginlib/class_list_macros.h>
 
 PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_move_base,
-        tidyup_actions::ActionExecutorMoveBase,
+        tidyup_actions::ActionExecutorDriveThroughDoor,
         continual_planning_executive::ActionExecutorInterface)
 
 namespace tidyup_actions
 {
 
-    bool ActionExecutorMoveBase::fillGoal(move_base_msgs::MoveBaseGoal & goal,
+    bool ActionExecutorDriveThroughDoor::fillGoal(move_base_msgs::MoveBaseGoal & goal,
             const DurativeAction & a, const SymbolicState & current)
     {
+        goal.target_pose.header.stamp = ros::Time::now();
 
+        ROS_ASSERT(a.parameters.size() == 3);
+        string door = a.parameters[0];
+        string start_location = a.parameters[1];
+        string goal_location = a.parameters[2];
+
+        // extract nicer + warn.
+        Predicate p;
+        p.parameters.push_back(goal_location);
+        p.name = "frame-id";
+        if(!current.hasObjectFluent(p, &goal.target_pose.header.frame_id))
+            return false;
+        p.name = "x";
+        if(!current.hasNumericalFluent(p, &goal.target_pose.pose.position.x))
+            return false;
+        p.name = "y";
+        if(!current.hasNumericalFluent(p, &goal.target_pose.pose.position.y))
+            return false;
+        p.name = "z";
+        if(!current.hasNumericalFluent(p, &goal.target_pose.pose.position.z))
+            return false;
+        p.name = "qx";
+        if(!current.hasNumericalFluent(p, &goal.target_pose.pose.orientation.x))
+            return false;
+        p.name = "qy";
+        if(!current.hasNumericalFluent(p, &goal.target_pose.pose.orientation.y))
+            return false;
+        p.name = "qz";
+        if(!current.hasNumericalFluent(p, &goal.target_pose.pose.orientation.z))
+            return false;
+        p.name = "qw";
+        if(!current.hasNumericalFluent(p, &goal.target_pose.pose.orientation.w))
+            return false;
+
+        ROS_INFO_STREAM("Created goal for ActionExecutorROSNavigation as: " << goal);
+
+        return true;
     }
 
-    void ActionExecutorMoveBase::updateState(const actionlib::SimpleClientGoalState & actionReturnState,
+    void ActionExecutorDriveThroughDoor::updateState(const actionlib::SimpleClientGoalState & actionReturnState,
             const move_base_msgs::MoveBaseResult & result,
             const DurativeAction & a, SymbolicState & current)
     {
         ROS_INFO("MoveBase returned result");
         if(actionReturnState == actionlib::SimpleClientGoalState::SUCCEEDED) {
             ROS_INFO("MoveBase succeeded.");
-
+            ROS_ASSERT(a.parameters.size() == 3);
+            string door = a.parameters[0];
+            string start_location = a.parameters[1];
+            string goal_location = a.parameters[2];
+            current.setBooleanPredicate("base-at", start_location, false);
+            current.setBooleanPredicate("base-at", goal_location, true);
+            current.setBooleanPredicate("door-state-known", door, false);
+            string goal_room = "unknown_room";
+            Predicate p;
+            p.name = "location-in-room";
+            p.parameters.push_back(goal_location);
+            if(current.hasObjectFluent(p, &goal_room))
+                current.setObjectFluent("location-in-room", "robot_location", goal_room);
         }
     }
 
--- tidyup_actions/src/actionExecutorPickupObject.cpp
+++ tidyup_actions/src/actionExecutorPickupObject.cpp
@@ -1,27 +1,106 @@
-#include "tidyup_actions/actionExecutorGraspObject.h"
+#include "tidyup_actions/actionExecutorPickupObject.h"
 #include <pluginlib/class_list_macros.h>
 
-PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_grasp_object,
-        tidyup_actions::ActionExecutorGraspObject,
+PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_pickup_object,
+        tidyup_actions::ActionExecutorPickupObject,
         continual_planning_executive::ActionExecutorInterface)
 
 namespace tidyup_actions
 {
 
-    bool ActionExecutorGraspObject::fillGoal(tidyup_msgs::GraspObjectGoal & goal,
+    bool ActionExecutorPickupObject::fillGoal(tidyup_msgs::GraspObjectGoal & goal,
             const DurativeAction & a, const SymbolicState & current)
     {
+        ROS_ASSERT(a.parameters.size() == 4);
+        string location = a.parameters[0];
+        string object = a.parameters[1];
+        string static_object = a.parameters[2];
+        string arm = a.parameters[3];
 
+        // set arm
+        goal.left_arm = false;
+        goal.right_arm = false;
+        if(arm == "left_arm") {
+            goal.left_arm = true;
+        }
+        if(arm == "right_arm") {
+            goal.right_arm = true;
+        }
+
+        // set target
+        goal.target.name = object;
+        goal.target.reachable_left_arm = false;
+        goal.target.reachable_right_arm = false;
+
+        // set the target pose from state
+        Predicate p;
+        p.name = "at-object";
+        p.parameters.push_back(object);
+        p.name = "frame-id";
+        if(!current.hasObjectFluent(p, &goal.target.pose.header.frame_id))
+            return false;
+        p.name = "timestamp";
+        double ts;
+        if(!current.hasNumericalFluent(p, &ts))
+            return false;
+        goal.target.pose.header.stamp = ros::Time(ts);
+        p.name = "x";
+        if(!current.hasNumericalFluent(p, &goal.target.pose.pose.position.x))
+            return false;
+        p.name = "y";
+        if(!current.hasNumericalFluent(p, &goal.target.pose.pose.position.y))
+            return false;
+        p.name = "z";
+        if(!current.hasNumericalFluent(p, &goal.target.pose.pose.position.z))
+            return false;
+        p.name = "qx";
+        if(!current.hasNumericalFluent(p, &goal.target.pose.pose.orientation.x))
+            return false;
+        p.name = "qy";
+        if(!current.hasNumericalFluent(p, &goal.target.pose.pose.orientation.y))
+            return false;
+        p.name = "qz";
+        if(!current.hasNumericalFluent(p, &goal.target.pose.pose.orientation.z))
+            return false;
+        p.name = "qw";
+        if(!current.hasNumericalFluent(p, &goal.target.pose.pose.orientation.w))
+            return false;
+
+        // set target reachable from state
+        p.name = "graspable-from";
+        p.parameters.push_back(location);
+
+        p.parameters.push_back("left_arm");
+        bool reachableLeft = false;
+        current.hasBooleanPredicate(p, &reachableLeft);
+        goal.target.reachable_left_arm = reachableLeft;
+        p.parameters[2] = "right_arm";
+        bool reachableRight = false;
+        current.hasBooleanPredicate(p, &reachableRight);
+        goal.target.reachable_right_arm = reachableRight;
+
+        return (goal.left_arm || goal.right_arm);
     }
 
-    void ActionExecutorGraspObject::updateState(const actionlib::SimpleClientGoalState & actionReturnState,
+    void ActionExecutorPickupObject::updateState(const actionlib::SimpleClientGoalState & actionReturnState,
             const tidyup_msgs::GraspObjectResult & result,
             const DurativeAction & a, SymbolicState & current)
     {
-        ROS_INFO("GraspObject returned result");
+        ROS_INFO("PickupObject returned result");
         if(actionReturnState == actionlib::SimpleClientGoalState::SUCCEEDED) {
-            ROS_INFO("GraspObject succeeded.");
-
+            ROS_INFO("PickupObject succeeded.");
+            ROS_ASSERT(a.parameters.size() == 4);
+            string location = a.parameters[0];
+            string object = a.parameters[1];
+            string static_object = a.parameters[2];
+            string arm = a.parameters[3];
+            current.setObjectFluent("arm-state", arm, "arm_unknown");
+            current.setBooleanPredicate("searched", location, false);
+            current.setBooleanPredicate("grasped", object + " " + arm, true);
+            current.setBooleanPredicate("on", object + " " + static_object, false);
+            current.setBooleanPredicate("graspable-from", object + " " + location + " left_arm", false);
+            current.setBooleanPredicate("graspable-from", object + " " + location + " right_arm", false);
+            // TODO: set false for other locations as well?
         }
     }
 
