86fb0a6739dfcac2790fc9aa9c3284d64eeac9f9
c.dornhege <c.dornhege@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2012-06-26 15:47:47 +0000

    generalized actionExecutorArmToSide and moved it to tidyup_actions

--- branches/symbolic_planning-experimental/tidyup_actions/include/tidyup_actions/actionExecutorArmToSide.h
+++ branches/symbolic_planning-experimental/tidyup_actions/include/tidyup_actions/actionExecutorArmToSide.h
@@ -5,18 +5,32 @@
 #include "continual_planning_executive/symbolicState.h"
 #include <tidyup_msgs/ArmToSideAction.h>
 
-namespace tidyup_place_actions
+namespace tidyup_actions
 {
 
     class ActionExecutorArmToSide : public ActionExecutorActionlib<tidyup_msgs::ArmToSideAction,
     tidyup_msgs::ArmToSideGoal, tidyup_msgs::ArmToSideResult>
     {
         public:
+            /**
+             * Initialize the ArmToSide action using the following parameters:
+             * action_plan_name action_server_name [armStatePredicate [armAtSideConstant]]
+             *
+             * armStatePredicate and armAtSideConstant give the predicate that is set to the at side
+             * constant when the action succeeds.
+             */
+            virtual void initialize(const std::deque<std::string> & arguments);
+
             virtual bool fillGoal(tidyup_msgs::ArmToSideGoal & goal,
                     const DurativeAction & a, const SymbolicState & current);
 
             virtual void updateState(const actionlib::SimpleClientGoalState & actionReturnState, const tidyup_msgs::ArmToSideResult & result,
                     const DurativeAction & a, SymbolicState & current);
+
+        private:
+            std::string _armStatePredicateName;     // the arm state predicate name
+            std::string _armAtSideConstantName;     // the arm at side position constant name
+
     };
 
 };
