4edbddde8f24ff5015d9e93810937fbee085605e
c.dornhege <c.dornhege@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2012-06-27 11:04:20 +0000

    generalized actionExecutorROSNavigation

--- branches/symbolic_planning-experimental/planner_navigation_actions/include/planner_navigation_actions/actionExecutorROSNavigation.h
+++ branches/symbolic_planning-experimental/planner_navigation_actions/include/planner_navigation_actions/actionExecutorROSNavigation.h
@@ -4,6 +4,8 @@
 #include "continual_planning_executive/actionExecutorActionlib.hpp"
 #include "continual_planning_executive/symbolicState.h"
 #include <move_base_msgs/MoveBaseAction.h>
+#include <vector>
+#include <utility>
 
 namespace planner_navigation_actions
 {
@@ -12,12 +14,28 @@ namespace planner_navigation_actions
                                         , move_base_msgs::MoveBaseGoal, move_base_msgs::MoveBaseResult>
     {
         public:
+            /**
+             * Parameters:
+             * action_plan_name action_server_name 
+             *      [start (<predicate> true|false)+] [goal (<predicate> true|false)+]
+             * Ex: drive /move_base start visited true goal exploring true 
+             */
+            virtual void initialize(const std::deque<std::string> & arguments);
+
             virtual bool fillGoal(move_base_msgs::MoveBaseGoal & goal,
                     const DurativeAction & a, const SymbolicState & current);
 
             virtual void updateState(const actionlib::SimpleClientGoalState & actionReturnState,
                     const move_base_msgs::MoveBaseResult & result,
                     const DurativeAction & a, SymbolicState & current);
+
+        protected:
+            ///< predicates to set/unset for the start location on successfull execution
+            std::vector<std::pair<std::string, bool> > _startPredicates;
+
+            ///< predicates to set/unset for the goal location on successfull execution
+            std::vector<std::pair<std::string, bool> > _goalPredicates;
+
     };
 
 };
--- branches/symbolic_planning-experimental/planner_navigation_actions/src/actionExecutorROSNavigation.cpp
+++ branches/symbolic_planning-experimental/planner_navigation_actions/src/actionExecutorROSNavigation.cpp
@@ -8,6 +8,46 @@ PLUGINLIB_DECLARE_CLASS(planner_navigation_actions, action_executor_ros_navigati
 namespace planner_navigation_actions
 {
 
+    void ActionExecutorROSNavigation::initialize(const std::deque<std::string> & arguments)
+    {
+        ActionExecutorActionlib<move_base_msgs::MoveBaseAction, move_base_msgs::MoveBaseGoal,
+            move_base_msgs::MoveBaseResult>::initialize(arguments);
+
+        if(arguments.size() < 3)
+            return;
+
+        bool parseStart = true;
+        if(arguments[2] == "goal") {
+            parseStart = false;
+        } else {
+            ROS_ASSERT(arguments[2] == "start");
+        }
+
+        unsigned int curArg = 3;
+        while(curArg < arguments.size()) {
+            if(arguments[curArg] == "goal") {
+                parseStart = false;
+                curArg++;
+                continue;
+            }
+            ROS_ASSERT(arguments.size() > curArg + 2);  // need to access curArg, curArg+1
+            string pred = arguments[curArg];
+            string setS = arguments[curArg + 1];
+            bool set = false;
+            if(setS == "true")
+                set = true;
+            else if(setS == "false")
+                set = false;
+            else
+                ROS_ASSERT(false);
+            if(parseStart)
+                _startPredicates.push_back(std::make_pair(pred, set));
+            else
+                _goalPredicates.push_back(std::make_pair(pred, set));
+            curArg += 2;
+        }
+    }
+
     bool ActionExecutorROSNavigation::fillGoal(move_base_msgs::MoveBaseGoal & goal,
             const DurativeAction & a, const SymbolicState & current)
     {
@@ -57,10 +97,17 @@ namespace planner_navigation_actions
     {
         if(actionReturnState == actionlib::SimpleClientGoalState::SUCCEEDED) {
             ROS_ASSERT(a.parameters.size() == 2);
+            string startName = a.parameters[0];
             string targetName = a.parameters[1];
-            current.setBooleanPredicate("explored", targetName, true);
-            // TODO fixme generic
-            current.setAllBooleanPredicates("recent-detected-objects", false);
+            for(std::vector<std::pair<std::string, bool> >::iterator it = _startPredicates.begin();
+                    it != _startPredicates.end(); it++) {
+                current.setBooleanPredicate(it->first, startName, it->second);
+            }
+            for(std::vector<std::pair<std::string, bool> >::iterator it = _goalPredicates.begin();
+                    it != _goalPredicates.end(); it++) {
+                current.setBooleanPredicate(it->first, targetName, it->second);
+            }
+
         }
     }
 
