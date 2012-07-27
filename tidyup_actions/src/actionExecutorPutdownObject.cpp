050b1e664eb5b7ab99c4e4817bc4576d8e63619d
Andreas Hertle <ahertle@users.noreply.github.com>
2012-07-27 12:06:18 +0000

    tidyup_actions: fixed dependecies (gki_utils->tidyup_utils), adopted putdown executor to new service type

--- tidyup_actions/src/actionExecutorPutdownObject.cpp
+++ tidyup_actions/src/actionExecutorPutdownObject.cpp
@@ -1,5 +1,9 @@
 #include "tidyup_actions/actionExecutorPutdownObject.h"
 #include <pluginlib/class_list_macros.h>
+#include "tidyup_msgs/GetPutdownPose.h"
+#include "arm_navigation_msgs/ArmNavigationErrorCodes.h"
+#include <arm_navigation_msgs/convert_messages.h>
+#include "tidyup_utils/planning_scene_interface.h"
 
 PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_putdown_object,
         tidyup_actions::ActionExecutorPutdownObject,
@@ -7,7 +11,6 @@ PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_putdown_object,
 
 namespace tidyup_actions
 {
-
     bool ActionExecutorPutdownObject::fillGoal(tidyup_msgs::PlaceObjectGoal & goal,
             const DurativeAction & a, const SymbolicState & current)
     {
@@ -20,14 +23,39 @@ namespace tidyup_actions
         // set arm
         goal.left_arm = false;
         goal.right_arm = false;
-        if(arm == "left_arm") {
+        if (arm == "left_arm")
+        {
             goal.left_arm = true;
         }
-        if(arm == "right_arm") {
+        if (arm == "right_arm")
+        {
             goal.right_arm = true;
         }
 
         goal.surface_name = static_object;
+
+        // TODO: reset planning scene to the state of world.
+        PlanningSceneInterface::instance()->resetPlanningScene();
+
+        tidyup_msgs::GetPutdownPose srv;
+        srv.request.static_object = static_object;
+        srv.request.arm = arm;
+        srv.request.putdown_object = object;
+        if (g_GetPutdownPose.call(srv))
+        {
+            if (srv.response.error_code.val == arm_navigation_msgs::ArmNavigationErrorCodes::SUCCESS)
+            {
+                ROS_INFO("Got a putdown pose.");
+                goal.position = srv.response.putdown_pose;
+            }
+            else
+            {
+                ROS_WARN("GetPutdownPose failed. Reason: %s (%d)",
+                        arm_navigation_msgs::armNavigationErrorCodeToString(srv.response.error_code).c_str(),
+                        srv.response.error_code.val);
+                return false;
+            }
+        }
 
         return (goal.left_arm || goal.right_arm);
     }
@@ -61,5 +89,29 @@ namespace tidyup_actions
         current.setObjectFluent("arm-state", arm, "arm_unknown");
         current.setBooleanPredicate("recent-detected-objects", location, false);
     }
+
+    bool ActionExecutorPutdownObject::fillPoseStamped(const string& poseName, const SymbolicState& state, geometry_msgs::PoseStamped& poseStamped)
+    {
+        Predicate p;
+        p.parameters.push_back(poseName);
+        bool no_error = true;
+        p.name = "x";
+        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.position.x);
+        p.name = "y";
+        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.position.y);
+        p.name = "z";
+        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.position.z);
+        p.name = "qx";
+        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.orientation.x);
+        p.name = "qy";
+        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.orientation.y);
+        p.name = "qz";
+        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.orientation.z);
+        p.name = "qw";
+        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.orientation.w);
+        poseStamped.header.frame_id = "/map";
+        return no_error;
+    }
+
 };
 
