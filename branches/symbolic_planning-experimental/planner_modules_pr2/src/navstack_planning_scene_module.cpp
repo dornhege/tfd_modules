55e900d0ff6ce4200ebe9581cc0e7034ee08ab03
a.hertle@gmail.com <a.hertle@gmail.com@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2012-07-27 13:08:36 +0000

    planner_modules_pr2: reverted navstack planning scene module to rev 2993

--- branches/symbolic_planning-experimental/planner_modules_pr2/src/navstack_planning_scene_module.cpp
+++ branches/symbolic_planning-experimental/planner_modules_pr2/src/navstack_planning_scene_module.cpp
@@ -3,8 +3,8 @@
 #include "hardcoded_facts/geometryPoses.h"
 #include "tidyup_utils/stringutil.h"
 #include <ros/ros.h>
-#include <geometry_msgs/Pose.h>
 #include <utility>
+#include <geometry_msgs/Pose.h>
 using std::map; using std::pair; using std::make_pair;
 
 VERIFY_CONDITIONCHECKER_DEF(planning_scene_pathCost);
@@ -100,8 +100,7 @@ bool PlanningSceneNavigationModule::setPlanningSceneDiffFromState(const Paramete
         numericalFluentCallbackType numericalFluentCallback)
 {
     // update objects in planning scene
-    arm_navigation_msgs::PlanningScene scene = PlanningSceneInterface::instance()->getPlanningScene();
-    vector<arm_navigation_msgs::CollisionObject>& objectList = scene.collision_objects;
+    vector<arm_navigation_msgs::CollisionObject>& objectList = spsdService.request.planning_scene_diff.collision_objects;
     vector<arm_navigation_msgs::CollisionObject>::iterator objectIterator = objectList.begin();
     for ( ; objectIterator != objectList.end(); objectIterator++)
     {
@@ -142,8 +141,8 @@ bool PlanningSceneNavigationModule::setPlanningSceneDiffFromState(const Paramete
     // set robot state
     ROS_ASSERT(parameterList.size() > 1);
     const string& robotStartPose = parameterList[0].value;
-    fillPoseFromState(scene.robot_state.multi_dof_joint_state.poses[0], robotStartPose, numericalFluentCallback);
-    return PlanningSceneInterface::instance()->setPlanningSceneDiff(scene);
+    fillPoseFromState(spsdService.request.planning_scene_diff.robot_state.multi_dof_joint_state.poses[0], robotStartPose, numericalFluentCallback);
+    return setPlanningSceneService.call(spsdService);
 }
 
 void PlanningSceneNavigationModule::initModule(int argc, char** argv)
@@ -153,16 +152,36 @@ void PlanningSceneNavigationModule::initModule(int argc, char** argv)
     g_NodeHandle->getParam("/continual_planning_executive/door_location_file", doorLocationFileName);
     loadDoorPoses(doorLocationFileName);
 
+    // init service for planning scene
+    std::string service_name = "/environment_server/set_planning_scene_diff";
+    while (!ros::service::waitForService(service_name, ros::Duration(3.0)))
+    {
+        ROS_ERROR("Service %s not available - waiting.", service_name.c_str());
+    }
+    setPlanningSceneService = g_NodeHandle->serviceClient<arm_navigation_msgs::SetPlanningSceneDiff>(service_name, true);
+    if (!setPlanningSceneService)
+    {
+        ROS_FATAL("Could not initialize get plan service from %s (client name: %s)", service_name.c_str(), setPlanningSceneService.getService().c_str());
+    }
     // init arm joint states
     armStates.push_back(ArmState("right_arm", "/arm_configurations/side_tuck/position/right_arm"));
     armStates.push_back(ArmState("left_arm", "/arm_configurations/side_tuck/position/left_arm"));
-    arm_navigation_msgs::PlanningScene scene = PlanningSceneInterface::instance()->getPlanningScene();
-    // replace arm joint states
-    for (size_t i = 0; i <= armStates.size(); i++)
+
+    // init planning scene
+    if (setPlanningSceneService.call(spsdService))
     {
-        armStates[i].replaceJointPositions(scene.robot_state.joint_state);
+        spsdService.request.planning_scene_diff = spsdService.response.planning_scene;
     }
-    PlanningSceneInterface::instance()->setPlanningSceneDiff(scene);
+    else
+    {
+        ROS_ERROR("%s Could not initialize planning scene.", __PRETTY_FUNCTION__);
+    }
+
+    // replace arm joint states
+    for (int i = RIGHT_ARM_AT_SIDE; i <= LEFT_ARM_AT_SIDE; i++)
+    {
+        armStates[i].replaceJointPositions(spsdService.request.planning_scene_diff.robot_state.joint_state);
+    }
 
     ROS_INFO("Initialized planning scene navstack module.\n");
 }
