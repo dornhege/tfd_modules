25ea124bd763d8d57ac72cc2c02e303a285f469f
a.hertle@gmail.com <a.hertle@gmail.com@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2012-08-09 14:18:59 +0000

    tidyup_actions: adoptations in executors/creator to new service definitions

--- branches/symbolic_planning-experimental/tidyup_actions/src/actionExecutorPutdownObject.cpp
+++ branches/symbolic_planning-experimental/tidyup_actions/src/actionExecutorPutdownObject.cpp
@@ -11,55 +11,72 @@ PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_putdown_object,
 
 namespace tidyup_actions
 {
+//    void ActionExecutorPutdownObject::initialize(const std::deque<std::string> & arguments)
+//    {
+//        super::initialize(arguments);
+//        ROS_ASSERT_MSG(arguments.size() >= 3, "ActionExecutorPutdownObject: service name argument is missing");
+//        ros::NodeHandle nh;
+//        getPutdownPoseService = nh.serviceClient<tidyup_msgs::GetPutdownPose>(arguments[2]);
+//        getPutdownPoseService.waitForExistence();
+//        ROS_INFO("ActionExecutorPutdownObject initialized.");
+//    }
+
     bool ActionExecutorPutdownObject::fillGoal(tidyup_msgs::PlaceObjectGoal & goal,
             const DurativeAction & a, const SymbolicState & current)
     {
         ROS_ASSERT(a.parameters.size() == 4);
         string location = a.parameters[0];
-        string object = a.parameters[1];
-        string static_object = a.parameters[2];
-        string arm = a.parameters[3];
+        goal.putdown_object = a.parameters[1];
+        goal.static_object = a.parameters[2];
+        goal.arm = a.parameters[3];
+        return true;
 
-        // set arm
-        goal.left_arm = false;
-        goal.right_arm = false;
-        if (arm == "left_arm")
-        {
-            goal.left_arm = true;
-        }
-        if (arm == "right_arm")
-        {
-            goal.right_arm = true;
-        }
-
-        goal.surface_name = static_object;
-
-        // reset planning scene to the state of world.
-        PlanningSceneInterface::instance()->resetPlanningScene();
-
-        tidyup_msgs::GetPutdownPose srv;
-        srv.request.static_object = static_object;
-        srv.request.arm = arm;
-        srv.request.putdown_object = object;
-        if (g_GetPutdownPose.call(srv))
-        {
-            if (srv.response.error_code.val == arm_navigation_msgs::ArmNavigationErrorCodes::SUCCESS)
-            {
-                ROS_INFO("Got a putdown pose.");
-                goal.position = srv.response.putdown_pose;
-            }
-            else
-            {
-                ROS_WARN("GetPutdownPose failed. Reason: %s (%d)",
-                        arm_navigation_msgs::armNavigationErrorCodeToString(srv.response.error_code).c_str(),
-                        srv.response.error_code.val);
-                return false;
-            }
-        }
-        // set the obtained putdown pose in goal
-        goal.position = srv.response.putdown_pose;
-
-        return (goal.left_arm || goal.right_arm);
+//        // set arm
+//        goal.left_arm = false;
+//        goal.right_arm = false;
+//        if (arm == "left_arm")
+//        {
+//            goal.left_arm = true;
+//        }
+//        if (arm == "right_arm")
+//        {
+//            goal.right_arm = true;
+//        }
+//
+//        goal.surface_name = static_object;
+//
+//        // reset planning scene to the state of world.
+//        PlanningSceneInterface::instance()->resetPlanningScene();
+//
+////        if (! getPutdownPoseService.isValid())
+////        {
+////            getPutdownPoseService =
+////        }
+//        tidyup_msgs::GetPutdownPose srv;
+//        srv.request.static_object = static_object;
+//        srv.request.arm = arm;
+//        srv.request.putdown_object = object;
+//        if (! getPutdownPoseService.call(srv))
+//        {
+//            ROS_ERROR("find putdown pose: service call failed.");
+//            return false;
+//        }
+//        if (srv.response.error_code.val == arm_navigation_msgs::ArmNavigationErrorCodes::SUCCESS)
+//        {
+//            ROS_INFO("Got a putdown pose.");
+//            goal.position = srv.response.putdown_pose;
+//        }
+//        else
+//        {
+//            ROS_WARN("GetPutdownPose failed. Reason: %s (%d)",
+//                    arm_navigation_msgs::armNavigationErrorCodeToString(srv.response.error_code).c_str(),
+//                    srv.response.error_code.val);
+//            return false;
+//        }
+//        // set the obtained putdown pose in goal
+//        goal.position = srv.response.putdown_pose;
+//
+//        return (goal.left_arm || goal.right_arm);
     }
 
     void ActionExecutorPutdownObject::updateState(const actionlib::SimpleClientGoalState & actionReturnState,
@@ -78,15 +95,15 @@ namespace tidyup_actions
             current.setBooleanPredicate("on", object + " " + static_object, true);
             current.setBooleanPredicate("graspable-from", object + " " + location + " " + arm, true);
             current.setBooleanPredicate("searched", location, false);
-            current.setNumericalFluent("x", object, result.position.pose.position.x);
-            current.setNumericalFluent("y", object, result.position.pose.position.y);
-            current.setNumericalFluent("z", object, result.position.pose.position.z);
-            current.setNumericalFluent("qx", object, result.position.pose.orientation.x);
-            current.setNumericalFluent("qy", object, result.position.pose.orientation.y);
-            current.setNumericalFluent("qz", object, result.position.pose.orientation.z);
-            current.setNumericalFluent("qw", object, result.position.pose.orientation.w);
-            current.setNumericalFluent("timestamp", object, result.position.header.stamp.sec);
-            current.setObjectFluent("frame-id", object, result.position.header.frame_id);
+            current.setNumericalFluent("x", object, result.putdown_pose.pose.position.x);
+            current.setNumericalFluent("y", object, result.putdown_pose.pose.position.y);
+            current.setNumericalFluent("z", object, result.putdown_pose.pose.position.z);
+            current.setNumericalFluent("qx", object, result.putdown_pose.pose.orientation.x);
+            current.setNumericalFluent("qy", object, result.putdown_pose.pose.orientation.y);
+            current.setNumericalFluent("qz", object, result.putdown_pose.pose.orientation.z);
+            current.setNumericalFluent("qw", object, result.putdown_pose.pose.orientation.w);
+            current.setNumericalFluent("timestamp", object, result.putdown_pose.header.stamp.sec);
+            current.setObjectFluent("frame-id", object, result.putdown_pose.header.frame_id);
         }
         current.setObjectFluent("arm-state", arm, "arm_unknown");
         current.setBooleanPredicate("recent-detected-objects", location, false);
