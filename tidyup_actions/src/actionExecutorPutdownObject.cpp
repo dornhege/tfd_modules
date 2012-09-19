4254b47f89e8938001146ca24b09d657014d968a
Andreas Hertle <ahertle@users.noreply.github.com>
2012-09-19 13:25:22 +0000

    tidyup_actions: removed dead code in putdown executor planner_modules_pr2: putdown module minor code refactoring and improvement in service call handling tidyup_arm_services: fixed putdown action execution result handling

--- tidyup_actions/src/actionExecutorPutdownObject.cpp
+++ tidyup_actions/src/actionExecutorPutdownObject.cpp
@@ -11,16 +11,6 @@ PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_putdown_object,
 
 namespace tidyup_actions
 {
-//    void ActionExecutorPutdownObject::initialize(const std::deque<std::string> & arguments)
-//    {
-//        super::initialize(arguments);
-//        ROS_ASSERT_MSG(arguments.size() >= 3, "ActionExecutorPutdownObject: service name argument is missing");
-//        ros::NodeHandle nh;
-//        getPutdownPoseService = nh.serviceClient<tidyup_msgs::GetPutdownPose>(arguments[2]);
-//        getPutdownPoseService.waitForExistence();
-//        ROS_INFO("ActionExecutorPutdownObject initialized.");
-//    }
-
     bool ActionExecutorPutdownObject::fillGoal(tidyup_msgs::PlaceObjectGoal & goal,
             const DurativeAction & a, const SymbolicState & current)
     {
@@ -30,53 +20,6 @@ namespace tidyup_actions
         goal.static_object = a.parameters[2];
         goal.arm = a.parameters[3];
         return true;
-
-//        // set arm
-//        goal.left_arm = false;
-//        goal.right_arm = false;
-//        if (arm == "left_arm")
-//        {
-//            goal.left_arm = true;
-//        }
-//        if (arm == "right_arm")
-//        {
-//            goal.right_arm = true;
-//        }
-//
-//        goal.surface_name = static_object;
-//
-//        // reset planning scene to the state of world.
-//        PlanningSceneInterface::instance()->resetPlanningScene();
-//
-////        if (! getPutdownPoseService.isValid())
-////        {
-////            getPutdownPoseService =
-////        }
-//        tidyup_msgs::GetPutdownPose srv;
-//        srv.request.static_object = static_object;
-//        srv.request.arm = arm;
-//        srv.request.putdown_object = object;
-//        if (! getPutdownPoseService.call(srv))
-//        {
-//            ROS_ERROR("find putdown pose: service call failed.");
-//            return false;
-//        }
-//        if (srv.response.error_code.val == arm_navigation_msgs::ArmNavigationErrorCodes::SUCCESS)
-//        {
-//            ROS_INFO("Got a putdown pose.");
-//            goal.position = srv.response.putdown_pose;
-//        }
-//        else
-//        {
-//            ROS_WARN("GetPutdownPose failed. Reason: %s (%d)",
-//                    arm_navigation_msgs::armNavigationErrorCodeToString(srv.response.error_code).c_str(),
-//                    srv.response.error_code.val);
-//            return false;
-//        }
-//        // set the obtained putdown pose in goal
-//        goal.position = srv.response.putdown_pose;
-//
-//        return (goal.left_arm || goal.right_arm);
     }
 
     void ActionExecutorPutdownObject::updateState(const actionlib::SimpleClientGoalState & actionReturnState,
@@ -108,29 +51,5 @@ namespace tidyup_actions
         current.setObjectFluent("arm-state", arm, "arm_unknown");
         current.setBooleanPredicate("recent-detected-objects", location, false);
     }
-
-    bool ActionExecutorPutdownObject::fillPoseStamped(const string& poseName, const SymbolicState& state, geometry_msgs::PoseStamped& poseStamped)
-    {
-        Predicate p;
-        p.parameters.push_back(poseName);
-        bool no_error = true;
-        p.name = "x";
-        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.position.x);
-        p.name = "y";
-        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.position.y);
-        p.name = "z";
-        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.position.z);
-        p.name = "qx";
-        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.orientation.x);
-        p.name = "qy";
-        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.orientation.y);
-        p.name = "qz";
-        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.orientation.z);
-        p.name = "qw";
-        no_error &= state.hasNumericalFluent(p, &poseStamped.pose.orientation.w);
-        poseStamped.header.frame_id = "/map";
-        return no_error;
-    }
-
 };
 
