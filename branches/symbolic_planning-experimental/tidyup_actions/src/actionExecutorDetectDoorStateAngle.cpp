14c7ef2a1f862f7cca22376fb4ca74fca3375313
c.dornhege@googlemail.com <c.dornhege@googlemail.com@50323fd8-bfc5-11de-ac5a-05e7513655e2>
2012-09-28 14:16:13 +0000

    added actionExecutorDetectDoorStateAngle

---
+++ branches/symbolic_planning-experimental/tidyup_actions/src/actionExecutorDetectDoorStateAngle.cpp
@@ -0,0 +1,57 @@
+#include "tidyup_actions/actionExecutorDetectDoorStateAngle.h"
+#include <pluginlib/class_list_macros.h>
+#include "tidyup_utils/planning_scene_interface.h"
+
+PLUGINLIB_DECLARE_CLASS(tidyup_actions, action_executor_detect_door_state_angle,
+        tidyup_actions::ActionExecutorDetectDoorStateAngle,
+        continual_planning_executive::ActionExecutorInterface)
+
+namespace tidyup_actions
+{
+
+    void ActionExecutorDetectDoorStateAngle::initialize(const std::deque<std::string> & arguments)
+    {
+        ActionExecutorService<tidyup_msgs::DoorState>::initialize(arguments);
+    }
+
+    bool ActionExecutorDetectDoorStateAngle::fillGoal(tidyup_msgs::DoorState::Request & goal,
+            const DurativeAction & a, const SymbolicState & current)
+    {
+        if(!PlanningSceneInterface::instance()->resetPlanningScene())   // FIXME try anyways?
+            ROS_ERROR("%s: PlanningScene reset failed.", __PRETTY_FUNCTION__);
+
+        return true;
+    }
+
+    void ActionExecutorDetectDoorStateAngle::updateState(bool success, tidyup_msgs::DoorState::Response & response,
+            const DurativeAction & a, SymbolicState & current)
+    {
+        ROS_INFO("DetectDoorStateAngle returned result");
+        if(success) {
+            ROS_INFO("DetectDoorStateAngle succeeded, door_found is: %d, angle is: %f.", response.door_found, response.angle);
+            ROS_ASSERT(a.parameters.size() == 2);
+            string location = a.parameters[0];
+            string door = a.parameters[1];
+            bool open = false;
+            if(!response.door_found) {
+                open = true;
+                ROS_INFO("No door found - assuming it is open.");
+            } else {
+                if(response.angle >= response.DOOR_OPEN_ANGLE) {
+                    open = true;
+                    ROS_INFO("Angle was %f > %f - Door is open.", response.angle, response.DOOR_OPEN_ANGLE);
+                } else if(response.angle <= response.DOOR_CLOSED_ANGLE) {
+                    open = false;
+                    ROS_INFO("Angle was %f < %f - Door is closed.", response.angle, response.DOOR_CLOSED_ANGLE);
+                } else {
+                    ROS_ERROR("Angle was %f - Unidentified door state - assuming it is open!", response.angle);
+                    open = true;
+                }
+            }
+            current.setBooleanPredicate("door-state-known", door, true);
+            current.setBooleanPredicate("door-open", door, open);
+        }
+    }
+
+};
+
